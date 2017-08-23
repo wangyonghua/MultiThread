796dc017224a201fe3844ec9b437c20c493bd873
wangyonghua <wyh@2016>
2017-08-23 18:59:31 +0800

    阻塞队列实现

---
+++ threadtest/thread-study/src/cn/study/thread/concurrent/condition/BufferQueueTest.java
@@ -0,0 +1,93 @@
+package cn.study.thread.concurrent.condition;
+
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BufferQueueTest {
+	public static void main(String[] args) {
+		CustomBufferQueue customBufferQueue = new BufferQueueTest().new CustomBufferQueue();
+		for (int i = 0; i < 100; i++) {
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					customBufferQueue.put(new Random().nextInt(10000));
+				}
+			}).start();
+		}
+
+		for (int i = 0; i < 100; i++) {
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					customBufferQueue.get();
+				}
+			}).start();
+		}
+	}
+
+	class CustomBufferQueue {
+		private int[] array = new int[7];
+		private Lock lock = new ReentrantLock();
+		private Condition notFullCondition = lock.newCondition();
+		private Condition notEmptyCondition = lock.newCondition();
+		private int count, takeCount, putCount;
+
+		public void put(int c) {
+			try {
+				lock.lock();
+				if (count == array.length) {
+					try {
+						notFullCondition.await();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if (putCount == array.length)
+					putCount = 0;
+				array[putCount] = c;
+				count++;
+				System.out.println("write index:" + putCount + ",count:" + count + ",value:" + c);
+				putCount++;
+				this.notEmptyCondition.signal();
+			} finally {
+				lock.unlock();
+			}
+		}
+
+		public int get() {
+			lock.lock();
+			int value = 0;
+			try {
+				if (count == 0) {
+					try {
+						this.notEmptyCondition.await();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if (takeCount == array.length) {
+					takeCount = 0;
+				}
+
+				value = array[takeCount];
+				count--;
+				System.out.println("get index:" + takeCount + ",count:" + count + ",value:" + value);
+				takeCount++;
+				this.notFullCondition.signal();
+			} finally {
+				// TODO: handle finally clause
+				lock.unlock();
+			}
+			return value;
+		}
+	}
+
+}
